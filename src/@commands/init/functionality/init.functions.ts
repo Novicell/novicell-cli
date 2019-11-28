deb3882855d6fef51bf44e4e80c8aaf56a2ce4db
Juozas Rastenis <jra@novicell.dk>
2019-11-28 15:49:02 +0100

    NCFE-67: Added JSON files for Default NUXT options

--- src/@commands/init/functionality/init.functions.ts
+++ src/@commands/init/functionality/init.functions.ts
@@ -1,9 +1,14 @@
-import inquirer from 'inquirer';
-import chalk from 'chalk';
+// node imports
+import { spawn } from 'child_process';
 import path from 'path';
-import { copySync } from 'fs-extra';
-import * as fs from 'fs';
+
+// 3rd pt
+import inquirer from 'inquirer';
 import { create_file } from '@utils/index';
+
+// local
+import default_nuxt_answers from '../default.nuxt.answers.json';
+import optional_nuxt_answers from '../optional.nuxt.answers.json';
 
 export const initQuestions: () => Promise<any> = () =>
   inquirer.prompt({
@@ -14,17 +19,19 @@ export const initQuestions: () => Promise<any> = () =>
   });
 
 export const goWithDefault = async () => {
+  // const { SET_UP_ENV } = await setUpEnvironmentVars();
   const { INIT_PATH } = await choosePath();
 
   // webpack considers __dirname as novicell-cli/dist, therefore:
   const project_slash_resources = path.join(__dirname, '../resources/spa-cms-setup');
 
-  try {
-    copySync(project_slash_resources, path.join(process.cwd(), INIT_PATH));
-    console.log(chalk.green('Successfully ') + 'coppied SPA+CMS setup to your working dir.');
-  } catch (error) {
-    console.log(error);
-  }
+  // try {
+  //   copySync(project_slash_resources, path.join(process.cwd(), INIT_PATH));
+  //   console.log(chalk.green('Successfully ') + 'coppied SPA+CMS setup to your working dir.');
+  // } catch (error) {
+  //   console.log(error);
+  // }
+  setUpNuxtForTesting();
 };
 
 export const goWithManual = () => {
@@ -35,6 +42,36 @@ const choosePath = async (): Promise<any> =>
   inquirer.prompt({
     type: 'input',
     name: 'INIT_PATH',
-    message: 'Where should we initialize?',
+    message: 'Destination path: ',
     default: './',
   });
+
+const setUpNuxt = async (ask_extra: boolean): Promise<any> => {
+  console.log('Lets set up .env file');
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'INIT_PATH',
+      message: 'Destination path: ',
+      default: './',
+    },
+  ]);
+
+  if (ask_extra) {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'INIT_PATH',
+        message: 'Destination path: ',
+        default: './',
+      },
+    ]);
+  }
+};
+
+const setUpNuxtForTesting = async (): Promise<any> => {
+  const merged_answers = { ...optional_nuxt_answers, ...default_nuxt_answers };
+  console.log(merged_answers);
+
+  spawn('npx.cmd', ['create-nuxt-app', 'testas', `--answers=${JSON.stringify(merged_answers)}`], { stdio: 'inherit' });
+};
