02a1d494d19e2c872949337c66c1b36a826699dd
Juozas Rastenis <jra@novicell.dk>
2019-12-04 12:31:29 +0100

    NCFE-67: lint/build err's fix'd

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,9 @@ import program from 'commander';
 import { add, add_features } from '@commands/add';
 import { init } from '@commands/init';
 import { showcase_opts, didYouMean } from '@utils/index';
-import { version } from '../package.json';
 import { FeatureList } from '@models/feature.interface.js';
 
-program.version(version);
+program.version('x.x.x');
 
 export const root_commands: FeatureList = {
   add: { value: 'add', description: 'Adds a feature to an existing project' },
