af886ec4eb07ca636d40463ac7d36877901de22f
Juozas Rastenis <jra@novicell.dk>
2019-12-06 10:57:27 +0100

    debugging logs

--- resources/spa-cms-setup/store/index.ts
+++ resources/spa-cms-setup/store/index.ts
@@ -8,6 +8,7 @@ export const mutations: MutationTree<RootState> = {};
 export const actions: ActionTree<RootState, RootState> = {
   async nuxtServerInit({ dispatch }: { dispatch: any }, ctx) {
     const path = `${ctx.env.baseUrl}/`;
+    console.log(path);
     const fetchNavigation = dispatch('navigation/FETCH_NAVIGATION', { path }, { root: true });
     const fetchSettings = dispatch('site/FETCH_SETTINGS', { path });
     try {
